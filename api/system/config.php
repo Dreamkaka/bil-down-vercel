17d53b7cadabac11fd08632ff35ebe9de1bfd6c8
幻梦晓寒_kaka <97349507+Dreamkaka@users.noreply.github.com>
2024-02-04 22:59:15 +0800

    Update config.php

--- api/system/config.php
+++ api/system/config.php
@@ -1,5 +1,5 @@
 <?php
         $data = array(
-          'cookie' => 'SESSDATA=3ba287b5%2C1722006402%2Ca5657%2A11CjAIYRYOFZE3fCz1YvLySmpUA0reV-ViurwA-Ur79jN9NF5Lq-wAl1PBzRFBpFEIKKYSVl9lRDRsM2Q1ZE9KYy1aTFV1TnB1WmRKbnVkNWlkc255SlhLSl9kLS1pOERCWXN0SlJseVhzTGRjX0paMFpudGt5b3FPc2xjQ044XzI1Nktscm4xODVBIIEC'
+          'cookie' => 'buvid3=757245EE-C259-E4FA-684A-2782C81ED8CB60443infoc; b_nut=1707058660; i-wanna-go-back=-1; b_ut=7; b_lsid=106965109D_18D74A006C4; _uuid=51965FAB-108C7-A459-4B77-95BC1026CD4FE61073infoc; enable_web_push=DISABLE; header_theme_version=undefined; buvid4=31BFCE2C-9AB0-705A-783E-6E99CDBED6B761228-024020414-dQ4QSqWQFG8sI+CnqwyOdg%3D%3D; buvid_fp=e1753ca38c82f368a988379fef2f71c0; bili_jct=b3c376ede22041ca26db423a45807801; DedeUserID=516951032; DedeUserID__ckMd5=b25c2fe3e29b8e31; sid=q3x0rtow; bp_video_offset_516951032=894242019591847988; home_feed_column=4; browser_resolution=787-804'
         );
         ?>
